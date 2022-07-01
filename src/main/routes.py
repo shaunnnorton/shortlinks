8f7cc6efb0f84be04d57322638866c785ca9430e
Shaun Norton <shaunnorton87@gmail.com>
2022-06-30 19:39:08 -0700

    Started Structure

---
+++ src/main/routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template, redirect, request, url_for, flash, blueprints
+
+from src import app #, db
+
+main = Blueprint("main", __name__)
+
+@main.route("/")
+def default_redirect():
+    return redirect(url_for("main.Gen_route"))
+
+@main.route("/Gen")
+def Gen_route():
+    return "In Develpoment"
+
+@main.route("/<shorthand>")
+def redirect_function(shorthand):
+    return "In Development"
+
